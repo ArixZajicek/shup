8d78dcd7ac8cd8fe4d8a3874665ab0ff01d7631f
Daniel Lowe <daniel.lowe@latakoo.com>
2023-01-28 00:32:12 -0800

    cleaned up main php file

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
-#!/bin/bash
-################################################################################
-##                        ENVIRONMENT VARIABLES                               ##
-################################################################################
-
 <?php
 
 	header("Content-Type: text/x-shellscript");
 	header("Content-Disposition: attachment; filename=\"shup.sh\"");
+
+	echo "#!/bin/bash";
 
 	function msg($message = '') {
 		echo "echo '" . $message . "'\n";
@@ -19,11 +16,11 @@
 			array_push($scripts_available, substr($filename, 0, -3));
 		}
 	}
-
 	
 	if (isset($_GET['list'])) {
 		// Just listing the scripts available
 		msg('Available scripts are ' . implode(', ', $scripts_available) . '.');
+		exit;
 
 	} else if (isset($_GET['run'])) {
 		// Explicitly define running scripts
@@ -31,22 +28,25 @@
 			// Make sure script is valid
 			if (!in_array($script, $scripts_available)) {
 				msg('Unexpected script ' . $script . ' passed, aborting. Available scripts are ' . implode(', ', $scripts_available) . '.');
-				exit();
+				exit;
 			}
 
 			// Make sure no duplicates
 			if (in_array($script, $scripts_to_run)) {
 				msg('Script ' . $script . ' passed more than once, aborting.');
-				exit();
+				exit;
 			}
 
 			// Make sure github username is provided for keys script
 			if ($script == 'keys') {
 				if (empty($_GET['gh'])) {
 					msg('keys script requires a GitHub username to be passed."');
-					exit();
+					exit;
 				} else {
-					echo 'GH_USER=' . $_GET['gh'];
+					echo "################################################################################\n";
+					echo "##                        ENVIRONMENT VARIABLES                               ##\n";
+					echo "################################################################################\n";
+					echo "GH_USER=" . $_GET['gh'] . "\n";
 				}
 			}
 
@@ -59,7 +59,10 @@
 		foreach($scripts_available as $script) {
 			if ($script == 'keys') {
 				if (!empty($_GET['gh'])) {
-					echo 'GH_USER=' . $_GET['gh'];
+					echo "################################################################################\n";
+					echo "##                        ENVIRONMENT VARIABLES                               ##\n";
+					echo "################################################################################\n";
+					echo "GH_USER=" . $_GET['gh'] . "\n";
 					array_push($scripts_to_run, $script);
 				}
 			} else {
@@ -72,8 +75,7 @@
 		msg('No scripts passed! Available scripts are ' . implode(', ', $scripts_available) . '."');
 	} else {
 		foreach($scripts_to_run as $script) {
-			echo file_get_contents('./scripts/' . $script . '.sh');
-			echo("\n");
+			echo file_get_contents('./scripts/' . $script . '.sh') . "\n";
 		}
 	}
 ?>
