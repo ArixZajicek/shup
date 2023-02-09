14f9caabea361bbcc6fe9ec1a55bb89225c4c5a6
Daniel Lowe <daniel.lowe@latakoo.com>
2023-02-08 21:01:06 -0800

    Fixed silly output

--- scripts/keys.sh
+++ scripts/keys.sh
@@ -16,7 +16,9 @@ else
     # Make sure we can get the current keys first, otherwise we will want to fail the operation.
     RETRIEVED_KEYS=`curl -sS https://github.com/$GH_USER.keys`
     if [ $? -eq 0 ]; then
-        PRIOR_FILTERED_KEYS=`grep --ignore-case --invert-match "# GITHUB USER \"$GH_USER\"" ~/.ssh/authorized_keys`
+        if [ -f ~/.ssh/authorized_keys]; then
+            PRIOR_FILTERED_KEYS=`grep --ignore-case --invert-match "# GITHUB USER \"$GH_USER\"" ~/.ssh/authorized_keys`
+        fi
         (echo "$PRIOR_FILTERED_KEYS"; echo "$RETRIEVED_KEYS" | sed --expression "s/$/ # GITHUB USER \"$GH_USER\"/") | tee ~/.ssh/authorized_keys
         echo "Keys from GitHub user $GH_USER successfully updated."
     else
